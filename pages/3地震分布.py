cbeeadd277d9661c3b7ce7da60293136ef3155f9
wyt3721 <71181150+wyt3721@users.noreply.github.com>
2024-05-08 18:35:09 +0800

    Update 3地震分布.py

--- pages/3地震分布.py
+++ pages/3地震分布.py
@@ -33,8 +33,6 @@ def read_data():
     return data
 
 
-#     # st.map(data2, latitude='latitude', longitude='longitude')
-
 
 if st.session_state["authentication_status"]:
     st.header("2000-2023全球地震分布图")
@@ -45,14 +43,7 @@ if st.session_state["authentication_status"]:
     df = read_data()
     df['time'] = pd.to_datetime(df['time'])
     df['year'] = pd.DatetimeIndex(df['time']).year.astype('str')
-    # st.write(df.info())
-    # df_y = df.groupby('year')
-    # df_y = df.groupby(['year']).size().reset_index()
+
     df_y = df.loc[df['year'] == opinions]
-    # st.write(type(opinions))
-    # y23['latitude'] = y23['latitude'].astype('float')
-    # y23['longitude'] = y23['longitude'].astype('float')
-    # df_y = df_y.iloc[:, 1:3]
-    # st.write(df_y)
 
     st.map(df_y, latitude='latitude', longitude='longitude')

a6f93729bdc99a6e5a247a432ef162204b05b0bd
wyt3721 <71181150+wyt3721@users.noreply.github.com>
2024-05-14 07:58:53 +0800

    Update 3地震分布.py

--- pages/3地震分布.py
+++ pages/3地震分布.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import yaml
-from yaml.loader import SafeLoader
-import streamlit_authenticator as stauth
+# import yaml
+# from yaml.loader import SafeLoader
+# import streamlit_authenticator as stauth
 import time
 # from obspy.core import UTCDateTime
 from datetime import datetime
@@ -27,23 +27,23 @@ from datetime import datetime
 #     st.switch_page('app.py')
 
 
-    @st.cache_data
-    def read_data():
-        data = pd.read_csv("./data/2000-2023.csv")
-        return data
+@st.cache_data
+def read_data():
+    data = pd.read_csv("./data/2000-2023.csv")
+    return data
 
 
 
 # if st.session_state["authentication_status"]:
-    st.header("2000-2023全球地震分布图")
-    opinions = st.selectbox("请选择年份：", (
-                                            '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008',
-                                            '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017',
-                                            '2018', '2019', '2020', '2021', '2022', '2023'))
-    df = read_data()
-    df['time'] = pd.to_datetime(df['time'])
-    df['year'] = pd.DatetimeIndex(df['time']).year.astype('str')
+st.header("2000-2023全球地震分布图")
+opinions = st.selectbox("请选择年份：", (
+                                        '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008',
+                                        '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017',
+                                        '2018', '2019', '2020', '2021', '2022', '2023'))
+df = read_data()
+df['time'] = pd.to_datetime(df['time'])
+df['year'] = pd.DatetimeIndex(df['time']).year.astype('str')
 
-    df_y = df.loc[df['year'] == opinions]
+df_y = df.loc[df['year'] == opinions]
 
-    st.map(df_y, latitude='latitude', longitude='longitude')
+st.map(df_y, latitude='latitude', longitude='longitude')

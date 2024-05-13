3a2a940ee904c0a39addbb912dc6d92dfa8111ad
wyt3721 <71181150+wyt3721@users.noreply.github.com>
2024-05-13 18:40:14 +0800

    Update 5地震数据库.py

--- pages/5地震数据库.py
+++ pages/5地震数据库.py
@@ -24,7 +24,7 @@ elif st.session_state["authentication_status"] is True:
 
     st.header(':book:地震资料数据库')
     st.divider()
-    st.caption('查询您需要的地震资料,云端暂时只能使用sqlite数据库')
+    st.caption('查询您需要的地震资料,云端暂时不能使用数据库')
     st.sidebar.write(f'你好， *{st.session_state["name"]}*')
     st.sidebar.success('''欢迎使用本系统 :smile: :joy:
                            :email: wyt3721@outlook.com
@@ -34,7 +34,7 @@ elif st.session_state["authentication_status"] is True:
     # for percent_complete in range(100):
     #     time.sleep(0.2)
     #     my_bar.progress(percent_complete + 1)
-    s = st.selectbox('请选择数据库：', ['mysql','sqlite',  'postgresql'])
+    s = st.selectbox('请选择数据库：', ['mysql', 'sqlite',  'postgresql'])
 
     if s == 'sqlite':
         conn = st.connection('pets_db', type='sql')

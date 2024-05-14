391e2abbcc5da49457e23a6106ffa5ac792e28f3
wyt3721 <71181150+wyt3721@users.noreply.github.com>
2024-05-14 10:12:34 +0800

    Update 5地震数据库.py

--- pages/5地震数据库.py
+++ pages/5地震数据库.py
@@ -41,7 +41,7 @@ st.sidebar.success('''欢迎使用本系统 :smile: :joy:
 s = st.selectbox('请选择数据库：', ['mysql', 'sqlite',  'postgresql'])
 
 if s == 'sqlite':
-    conn = st.connection(url=st.secrtes["connections.pets_db"]["url"])
+    conn = st.connection(dial=st.secrtes["connections.pets_db"], type="sql")
     conn
     # engine = create_engine('env:url', echo=Ture)
     # engine
